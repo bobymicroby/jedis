0465b5bf64554e6a8f072f965b11482ff7a60f17
borislav ivanov <borislav.ivanov@redis.com>
2024-10-04 13:44:16 +0300

    fake performance regression

--- src/test/java/io/redis/examples/BitfieldExample.java
+++ src/test/java/io/redis/examples/BitfieldExample.java
@@ -35,7 +35,7 @@ public class BitfieldExample {
         System.out.println(res3);   // >>> [1450, 2]
 
       try {
-        Thread.sleep( 91 * 1000);
+        Thread.sleep( 92 * 1000);
       } catch (InterruptedException e) {
         throw new RuntimeException(e);
       }
