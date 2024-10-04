b6de0b6766aa8069e048f7060d17da5877d036fd
borislav ivanov <borislav.ivanov@redis.com>
2024-10-04 15:31:38 +0300

    introduce perf regression

--- src/test/java/io/redis/examples/BitfieldExample.java
+++ src/test/java/io/redis/examples/BitfieldExample.java
@@ -35,7 +35,7 @@ public class BitfieldExample {
         System.out.println(res3);   // >>> [1450, 2]
 
       try {
-        Thread.sleep( 20  * 1000);
+        Thread.sleep( 10  * 1000);
       } catch (InterruptedException e) {
         throw new RuntimeException(e);
       }
